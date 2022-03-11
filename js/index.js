a7b9ef408121f52af7a427804bbe7a72ba8ef01b
Svyatoslavpech <svyatoslavpech@gmail.com>
2022-03-11 11:36:34 -0700

    task 4 complete

--- js/index.js
+++ js/index.js
@@ -1,47 +1,38 @@
 //TASK 4
 const submitButton = document.getElementById('submit-button');
+const newTaskNameInput = document.querySelector('#task-name');
+const newAssignedToInput = document.querySelector('#assigned-person');
+const dateInput = document.querySelector('#due-date');
+const newDescriptionInput = document.querySelector('#task-description');
 
-// const newTask = {
-//   taskName: taskNameHTML,
-//   assignedTo: assignedToHTML,
-//   dueDate: dueDateHTML,
-  //description: descriptionHTML,
-  // AAA = () => {
-    
-  // }
-// }
 
 const validFormFieldInput = () => {
-  const newTaskNameInput = document.querySelector('#task-name');
-  const name = newTaskNameInput.value;
-
-  const newAssignedToInput = document.querySelector('#assigned-person');
-  const person = newAssignedToInput.value;
-
-  const dateInput = document.querySelector('#due-date');
-  const date = dateInput.value;
-
-  const newDescriptionInput = document.querySelector('#task-description');
-  const description = newDescriptionInput.value;
- 
-  var myAlert = document.querySelector('.liveAlertPlaceholder');
-  myAlert.innerHTML = '';
-
-  function warning (inputValue, inputName) {
-    if (inputValue.length === 0) {
-      myAlert.innerHTML = '<div class="alert alert-warning alert-dismissible" role="alert">' + 'Please input ' + inputName + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
-      
-      alertPlaceholder.append(myAlert);
-      } 
-  }
   
-  warning(name, 'task name');
-  warning(person, "a person's name");
-  warning(date, 'a date');
-  warning(description, 'a task description');
+  const name = newTaskNameInput.value;
+  const person = newAssignedToInput.value;
+  const date = dateInput.value;
+  const description = newDescriptionInput.value;
+
+
+  // var myAlert = document.querySelector('.liveAlertPlaceholder');
+  // myAlert.innerHTML = '';
+
+  // function warning (inputValue, inputName) {
+  //   if (inputValue.length === 0) {
+  //     myAlert.innerHTML = '<div class="alert alert-warning alert-dismissible" role="alert">' + 'Please input ' 
+  //     + inputName + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';      
+  //     alertPlaceholder.append(myAlert);
+  //     } 
+  // }
+  
+  // warning(name, 'task name');
+  // warning(person, "a person's name");
+  // warning(date, 'a date');
+  // warning(description, 'a task description');
 }
 
-submitButton.onclick = validFormFieldInput;
+const test = new TaskManager;
+test.addTask('aaa', 'fff', 'gggg', '01/01/2022');
 
-const myTask = new TaskManager;
-console.log(myTask.tasks)
+
+submitButton.addEventListener('click', validFormFieldInput);
