ba40cd7c0680ed1bf18e90b69119ebd3356ff334
Victoria <70860669+victoriafreder@users.noreply.github.com>
2022-03-12 14:06:22 -0600

    complete task 5

--- js/index.js
+++ js/index.js
@@ -6,12 +6,17 @@ const dateInput = document.querySelector('#due-date');
 const newDescriptionInput = document.querySelector('#task-description');
 const myTask = new TaskManager;
 
+  let taskName = newTaskNameInput.value;
+  let person = newAssignedToInput.value;
+  let date = dateInput.value;
+  let description = newDescriptionInput.value;
+
 const validFormFieldInput = () => {
   
-   const name = newTaskNameInput.value;
-   const person = newAssignedToInput.value;
-   const date = dateInput.value;
-   const description = newDescriptionInput.value;
+   taskName = newTaskNameInput.value;
+   person = newAssignedToInput.value;
+   date = dateInput.value;
+   description = newDescriptionInput.value;
 
    var myAlert = document.querySelector('.liveAlertPlaceholder');
    myAlert.innerHTML = '';
@@ -20,21 +25,28 @@ const validFormFieldInput = () => {
      if (inputValue.length === 0) {
        myAlert.innerHTML = '<div class="alert alert-warning alert-dismissible" role="alert">' + 'Please input ' 
        + inputName + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';      
-       alertPlaceholder.append(myAlert);
+      //  alertPlaceholder.append(myAlert);
        } 
    }
   
-   warning(name, 'task name');
+   warning(taskName, 'task name');
    warning(person, "a person's name");
    warning(date, 'a date');
    warning(description, 'a task description');
 
-  myTask.addTask(name, description, person, date, 'TODO');
+  // myTask.addTask(taskName, description, person, date, 'TODO');
 
 }
 
 //const test = new TaskManager;
 //test.addTask('aaa', 'fff', 'gggg', '01/01/2022');
 
-submitButton.addEventListener('click', validFormFieldInput);
+submitButton.addEventListener('click', function () {
+  validFormFieldInput();
+  if (taskName && person && date && description) {
+    myTask.addTask(taskName, person, date, description);
+    const taskForm = document.getElementById('task-form');
+    taskForm.reset();
+  }
+} );
 
